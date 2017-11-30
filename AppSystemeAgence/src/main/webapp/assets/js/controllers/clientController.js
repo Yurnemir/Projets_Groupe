435eedc6620b4fb22a31d41e373b439e125aa120
inti-0257 <inti-0257@192.168.1.167>
2017-11-30 14:06:26 +0100

    Kevin: 3fonctions vue Client (find all, get by id, add)

--- AppSystemeAgence/src/main/webapp/assets/js/controllers/clientController.js
+++ AppSystemeAgence/src/main/webapp/assets/js/controllers/clientController.js
@@ -1,12 +1,55 @@
 
 monApp.controller("listeClientsCtrl", function($scope, clientProvider) {
-	$scope.message = "listeClientsCtrl";
+	
+	clientProvider.getAllClients(function(callBack){
+		$scope.listeClients = callBack;
+	});
 });
 
 monApp.controller("rechercheClientCtrl", function($scope, clientProvider) {
-	$scope.message = "rechercheClientCtrl";
+	
+	$scope.clientFound = false;
+	$scope.clientID = 0;
+	
+	$scope.recherche = function(){
+		clientProvider.getClientById($scope.clientID,function(callBack){
+			if (callBack!=undefined && callBack != ""){
+				$scope.client=callBack;
+				$scope.clientFound = true;		
+			} else {
+				$scope.clientFound = false;
+			}
+		});
+	};
 });
 
 monApp.controller("ajoutClientCtrl", function($scope, clientProvider) {
-	$scope.message = "ajoutClientCtrl";
+	$scope.message = "";
+	
+	$scope.client={
+			nom : "",
+			telephone : ""
+	};
+	$scope.clientCreated = false;
+	
+	$scope.agentID = 0;
+	
+	$scope.ajout = function(){
+		clientProvider.addClient($scope.client,$scope.agentID,function(callBack){
+			if(callBack!=undefined && callBack != ""){
+				$scope.client = callBack;
+				$scope.clientCreated = true;
+				$scope.message = "";
+			} else {
+				$scope.clientCreated = false;
+				$scope.message="Pas de Client créé";
+			}
+		});
+	};
+	
+	
+	
+	
+	
+	
 });
--- AppSystemeAgence/src/main/webapp/assets/js/providers/clientProvider.js
+++ AppSystemeAgence/src/main/webapp/assets/js/providers/clientProvider.js
@@ -3,12 +3,44 @@ monApp.factory("clientProvider", function($http) {
 	var urlRacine = "http://localhost:8080/AppSystemeAgence";
 	
 	function getAllClientsFunction(callBack) {
+		//envoi de la requete
+		$http({
+			method:'GET',
+			url:urlRacine+"/client/all"
+		}).then(function success(response){
+			console.log(response.data);
+			
+			callBack(response.data);
+		},function error(response){
+			console.log("----- Erreur:"+response.statusText)
+		});
 	}
 	
-	function getClientByIdFunction(callBack) {
+	function getClientByIdFunction(id,callBack) {
+		$http({
+			method:'GET',
+			url:urlRacine+"/client/getById?clientID="+id
+		}).then(function success(response){
+			console.log(response.data);
+			callBack(response.data);
+		},function error(response){
+			console.log("----- Erreur:"+response.statusText);
+		});
 	}
 	
-	function addClientFunction(callBack) {
+	function addClientFunction(client,idAgent,callBack) {
+		$http({
+			method:'POST',
+			url:urlRacine+"/client/add?agentID="+idAgent,
+			headers:{
+				"Content-Type":"application/json"
+			},
+			data:angular.toJson(client)
+		}).then(function success(response){
+			callBack(response.data);
+		},function error(response){
+			console.log("----- Erreur:"+response.statusText);
+		})
 	}
 	
 	return {
